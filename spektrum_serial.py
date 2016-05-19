91a7342fa114c623797ab5495043b785704a75e1
david reid <zathrasorama@gmail.com>
2016-05-19 12:23:21 +0100

    Large update to try and improve the sync'ing and make the code a little simpler between files and serial.

--- spektrum_serial.py
+++ spektrum_serial.py
@@ -5,6 +5,7 @@ import sys
 import struct
 import serial
 import argparse
+import fcntl
 from struct import pack
 from time import time
 
@@ -17,8 +18,10 @@ class SpektrumFrame(object):
         self.channel_data = data[2:]
         self.channel_values = {}
         self.highest_channel = 0
+        self.raw = data
 
     def add_frame(self, frame):
+        self.raw.extend(frame)
         self.channel_data.extend(frame[2:])
 
     @property
@@ -27,10 +30,9 @@ class SpektrumFrame(object):
 
     @property
     def frames_required(self):
+        if self.channel_bits == 11:
+            return self.tx_data & 0x03
         return 2
-        # This would appear to be what should give the correct value,
-        # but it seems to fail too frequently!
-        # return self.tx_data & 0x03
 
     @property
     def tx_number(self):
@@ -71,7 +73,7 @@ Frames Required:      {}
 Highest Channel:      {}
   TX Number:          {}
   Upper 3 buts        0x{:02x}
-  Lower 2 bits        0x{:02x}""".format(self.dropped,
+  Lower 2 bits        0x{:02x}\n\n""".format(self.dropped,
                                          self.tx_data,
                                          bin(self.tx_data)[2:],
                                          self.channel_bits,
@@ -88,70 +90,166 @@ Highest Channel:      {}
         title += "\n" + "".join(["{:4d} ".format(self.channel_values.get(x, 0)) for x in chans])
         return title
 
-
-def read_bytes(inp, out=None):
-    bytes = map(ord, inp.read(16))
-    if out is not None and len(bytes) > 0:
-        out.write(pack("{}B".format(len(bytes)), *bytes))
-    return bytes if len(bytes) == 16 else None
-
-
-def read_loop(inp, out=None, summary=False):
-    total_frames = 0
+    def raw_bytes(self):
+        s = "  "
+        for n in range(len(self.raw)):
+            if n > 0 and n % 16 == 0:
+                s += "\n  "
+            s += "{:02X} ".format(self.raw[n])
+        return s
 
 
-    if hasattr(inp, 'inWaiting'):
+class SpektrumReader(object):
+    def __init__(self, inp, outp=None):
+        self.input = inp
+        self.output = outp
+        self.spektrum = None
+        self.total_frames = 0
+
+    def sync(self):
+        # A full set of data should be transmitted at least every 22ms
+        # so when we are looking to sync we want to see a repeating series
+        # of 32 bytes (2 16 byte frames seperated by 11ms).
+        offs = 0
+        done = False
+        bytes = self.read_bytes(64)
+        if bytes is None:
+            return False
+
+        while offs < 32 and bytes[offs] == 0xff:
+            bytes.extend(self.read_bytes(1))
+            offs += 1
+        
+        while offs < 32:
+            matched = True
+            if bytes[offs] != 0xff and bytes[offs] == bytes[offs + 16]:
+                for ck in range(32):
+                    if bytes[ck + offs] != bytes[ck + offs + 32]:
+                        matched = False
+                        break
+
+                if matched:
+                    if bytes[offs + 2] & 0x80 == 0x80:
+                        self.read_bytes(16)
+                    return True
+            offs += 1
+            bytes.extend(self.read_bytes(1))
+
+        return False
+
+    def read_bytes(self, n=16):
+        bytes = map(ord, self.input.read(n))
+        if len(bytes) == 0:
+            return None
+        if self.output is not None and len(bytes) > 0:
+            self.output.write(pack("{}B".format(len(bytes)), *bytes))
+        return bytes if len(bytes) == n else None
+
+    def build_spektrum(self):
+        self.spektrum = None
+        bytes = self.read_bytes(16)
+        if bytes is None:
+            return False
+
+        if bytes[2] & 0x80 == 0x80:
+            print("Appear to have detected a second frame as initial??? Resyncing...")
+            if self.sync() is False:
+                print("Unable to resync, aborting...")
+                return False
+            return self.build_spektrum()
+
+        self.spektrum = SpektrumFrame(bytes)
+        for extra in range(1, self.spektrum.frames_required):
+            bytes = self.read_bytes(16)
+            if bytes is None:
+                break
+            self.spektrum.add_frame(bytes)
+
+        if not self.spektrum.is_complete():
+            return False
+        self.spektrum.decode_channels()
+        self.total_frames += self.spektrum.frames_required
+        return True
+
+    def close_all(self):
+       if hasattr(self, 'close_input'):
+           self.close_input()
+       self.input.close()
+       if self.output:
+           self.output.close()
+
+    def print_data(self):
+        if self.spektrum is None:
+            return
+        if self.total_frames <= 2:
+            print("\n{}\n".format(self.spektrum.summary()))
+        print("\033[F\033[F\033[F\033[F")        
+        print(self.spektrum.channel_value_string())
+
+
+class SerialReader(SpektrumReader):
+    def __init__(self, serial, output=None):
+        SpektrumReader.__init__(self, serial, output)
+   
+    def read_loop(self, summary=False):
+        total_frames = 0
+
         print("Waiting for data to be available...")
         try:
-            while inp.inWaiting() == 0:
+            while self.input.inWaiting() == 0:
                 pass
         except KeyboardInterrupt:
             return
-        print("Reading data...\n")
+        print("Reading data... CTRL + C to exit\n")
 
-    last = time()
-    while True:
-        try:
-            if time() - last > 5 and total_frames > 0:
-                print("Break in reception detected. Resetting frame counter.")
-                total_frames = 0
+        last = time()
+        if self.sync() is False:
+            print("Unable to sync frames. Exiting...")
+            return
 
-            bytes = read_bytes(inp, out)
-            if bytes is None:
-                continue
+        while True:
+            try:
+                if time() - last > 5 and self.total_frames > 0:
+                    print("Break in reception detected. Resetting frame counter.")
+                    self.total_frames = 0
 
-            if bytes[1] & 0x03 == 0:
-                print("Likely invalid frame detected.")
-                continue
+                if self.build_spektrum() is False:
+                    continue
 
-            if bytes[2] & 0x80 == 0x80:
-                print("    probable second frame detected! [0x{:02X}]...".format(bytes[2]))
-                continue
-
-            sf = SpektrumFrame(bytes)
-            for extra in range(1, sf.frames_required):
-                bytes = read_bytes(inp, out)
-                if bytes is None:
-                    break
-                sf.add_frame(bytes)
-
-            if not sf.is_complete():
-                continue
-
-            sf.decode_channels()
-            if total_frames == 0:
-                print("\n{}\n".format(sf.summary()))
+                self.print_data()
                 if summary:
                     return
-            else:
-                print("\033[F\033[F\033[F\033[F")        
-                print(sf.channel_value_string())
-            total_frames += sf.frames_required
-            last = time()
-        except KeyboardInterrupt:
-            break
+                        
+                total_frames += sf.frames_required
+                last = time()
+            except KeyboardInterrupt:
+                break
 
-    print("\n\nTotal of {} frames received and processed.".format(total_frames))
+    def close_input(self):
+        self.input.flush()
+
+
+class FileReader(SpektrumReader):
+    def __init__(self, serial, output=None):
+        SpektrumReader.__init__(self, serial, output)
+   
+    def read_loop(self, summary=False):
+        highest = 0
+        if self.sync() is False:
+            return
+
+        while True:
+            if self.build_spektrum() is False:
+                break
+            
+            if highest > 0 and highest != self.spektrum.highest_channel:
+                self.total_frames = 0
+
+            self.print_data()
+            if summary:
+                return
+
+            highest = self.spektrum.highest_channel
 
 
 def main():
@@ -168,12 +266,13 @@ def main():
     
     if args.output is not None and args.serial is None:
         print("Output files can only be created when using serial devices.")
-    
+
+    reader = None
     if args.serial:
         if not os.path.exists(args.serial):
             print("The supplied serial path does not exist.")
             sys.exit(0)
-            
+        out = None            
         ser = serial.Serial(port=args.serial,
                             baudrate=115200,
                             parity='N',
@@ -184,26 +283,21 @@ def main():
         if not ser.isOpen():
             print("Unable to open the device.")
             sys.exit(0)
-
+        reader = SerialReader(ser)
         print("Opened serial port...")
-        if args.output:
-            output_fh = open(args.output, 'wb')
-            read_loop(ser, output_fh)
-            output_fh.close()
-        else:
-            read_loop(ser)
-
-        # should we flush any remaining bytes?
-        ser.close()
-
     elif args.file:
         if not os.path.exists(args.file):
             print("The supplied filename does not exist.")
-            sys.exit(0)
-        with open(args.file, 'rb') as fh:
-            read_loop(fh, summary=args.summary_only)
+            sys.exit(0)           
+        reader = FileReader(open(args.file, 'rb'))
 
+    if args.output:
+        reader.output = open(args.output, 'wb')
 
+    reader.read_loop(args.summary_only)
+    print("\nTotal of {} frames read.".format(reader.total_frames))
+    reader.close_all()
+    
 main()
 
 
